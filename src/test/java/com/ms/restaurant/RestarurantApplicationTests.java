10018fa0e71070839e978f1b0f6ce1cd9e4b2015
Jitender Yadav <jitender1137@gmail.com>
2024-04-14 12:36:55 +0530

    remove test

--- src/test/java/com/ms/restaurant/RestarurantApplicationTests.java
+++ src/test/java/com/ms/restaurant/RestarurantApplicationTests.java
@@ -1,13 +1,13 @@
-package com.ms.restaurant;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class RestarurantApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.ms.restaurant;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class RestarurantApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
